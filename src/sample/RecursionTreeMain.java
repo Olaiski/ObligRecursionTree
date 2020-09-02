4c0a3827cb9b5412517380303039815d13bfa2fc
Olaiski <kungbinders@gmail.com>
2020-09-02 20:33:45 +0200

    Final?

--- src/sample/RecursionTreeMain.java
+++ src/sample/RecursionTreeMain.java
@@ -9,26 +9,25 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
+import java.util.Random;
+
 public class RecursionTreeMain extends Application {
 
+    private final RightMenuVbox rBox = new RightMenuVbox();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
         RecursiveTreePane pane = new RecursiveTreePane();
-        RightMenuVbox rBox = new RightMenuVbox();
 
-
-        rBox.getBtnOk().setOnAction(e ->
-                pane.setTree(rBox.getOrder(), rBox.getLength())
-        );
-
-
+        rBox.getBtnOk().setOnAction(e -> {
+            pane.disco = rBox.getDiscoCb().isSelected();
+            pane.setTree(rBox.getOrder(), rBox.getLength(), rBox.getRandomSliderValue());
+        });
 
         BorderPane borderPane = new BorderPane();
         borderPane.setCenter(pane);
         borderPane.setRight(rBox);
-
 
         Scene scene = new Scene(borderPane, 700, 600);
         primaryStage.setTitle("RecTree");
@@ -43,36 +42,42 @@ public class RecursionTreeMain extends Application {
 
         private int order;
         private double len;
+        private double sliderRandomVal;
+        private boolean disco = false;
 
 
-        public void setTree(int order) {
-            this.order = order;
-            paint();
-        }
-
-        public void setTree(int order, double len){
+        public void setTree(int order, double len, double sliderRandomVal){
             this.order = order;
             this.len = len;
+            this.sliderRandomVal = sliderRandomVal;
+
+            if (disco) {
+                this.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+            } else{
+                this.setBackground(new Background(new BackgroundFill(Color.GRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+            }
+
             paint();
         }
+
 
 
         public RecursiveTreePane() {
             this.setBackground(new Background(new BackgroundFill(Color.GRAY, CornerRadii.EMPTY, Insets.EMPTY)));
-        }
 
+        }
 
         protected void paint() {
             Point2D p = new Point2D(getWidth() / 2, getHeight());
 
             this.getChildren().clear();
-            displayTree(order, p, -90, getHeight() / 4);
+            displayTree(order, p, -90, len);
         }
 
 
         /**
          *
-         * @param n number of recursive "iterations"
+         * @param n depth / number of recursive "iterations"
          * @param origin the x and y on where to start drawing
          * @param rotation degrees
          * @param length how long the line should be (pixels)
@@ -81,25 +86,107 @@ public class RecursionTreeMain extends Application {
 
             Point2D endPoint = endPoint(origin, rotation, length);
 
+            int red = (int) (Math.random() * 256);
+            int green = (int) (Math.random() * 256);
+            int blue = (int) (Math.random() * 256);
+
 
             if (n == 0) {
                 Line line = new Line(origin.getX(), origin.getY(), endPoint.getX(), endPoint.getY());
-                line.setStroke(Color.WHITE);
+
+                if (disco) line.setStroke(Color.rgb(red,green,blue)); else line.setStroke(Color.WHITE);
+
                 getChildren().add(line);
             }
             else if (length > 2) {
 
                 Line line = new Line(origin.getX(), origin.getY(), endPoint.getX(), endPoint.getY());
-                line.setStroke(Color.WHITE);
+
+                if (disco) line.setStroke(Color.rgb(red,green,blue)); else line.setStroke(Color.WHITE);
+
                 getChildren().add(line);
 
-                displayTree(n - 1, endPoint, rotation - 30, length * 0.67f);
-                displayTree(n - 1, endPoint, rotation + 30, length * 0.67f);
-
+                if(sliderRandomVal == 0.0) {
+                    displayTree(n - 1, endPoint, rotation - randomRotation(sliderRandomVal), length * randomLength(sliderRandomVal));
+                    displayTree(n - 1, endPoint, rotation + randomRotation(sliderRandomVal), length * randomLength(sliderRandomVal));
+                }
+                else {
+                    // Grein nr 1 blir blokkert, tegner grein 2
+                    if(randomNumberGen(1, 10) == 1) {
+                        displayTree(n - 1, endPoint, rotation + randomRotation(sliderRandomVal), length * randomLength(sliderRandomVal));
+                    }
+                    // Grein nr 2 blir blokkert, tegner grein 1
+                    else if(randomNumberGen(1, 10) == 1) {
+                        displayTree(n - 1, endPoint, rotation - randomRotation(sliderRandomVal), length * randomLength(sliderRandomVal));
+                    }
+                    // Ingen greiner blokkert, tegner begge.
+                    else {
+                        displayTree(n - 1, endPoint, rotation - randomRotation(sliderRandomVal), length * randomLength(sliderRandomVal));
+                        displayTree(n - 1, endPoint, rotation + randomRotation(sliderRandomVal), length * randomLength(sliderRandomVal));
+                    }
+                }
             }
-            
         }
 
+        private int randomRotation(double level) {
+            int number = 30;
+            switch((int) level) {
+                case 0:
+                    break;
+                case 1:
+                    number = randomNumberGen(25, 35);
+                    break;
+                case 2:
+                    number = randomNumberGen(20, 40);
+                    break;
+                case 3:
+                    number = randomNumberGen(15, 45);
+                    break;
+                default:
+            }
+            return number;
+        }
+
+
+        // Generate random number within a range
+        private int randomNumberGen(int low, int high) {
+            Random rn = new Random();
+            int range = high - low + 1;
+            return rn.nextInt(range) + low;
+        }
+
+        private float randomNumberGen(float low, float high) {
+            Random r = new Random();
+            return low + r.nextFloat() * (high - low);
+        }
+
+        private float randomLength(double level) {
+            float tall = 0.8f;
+            switch((int) level) {
+                case 0:
+                    break;
+                case 1:
+                    tall = randomNumberGen(0.7F, 0.8F);
+                    break;
+                case 2:
+                    tall = randomNumberGen(0.6F, 0.9F);
+                    break;
+                case 3:
+                    tall = randomNumberGen(0.5F, 1.1F);
+                    break;
+                default:
+            }
+            return tall;
+        }
+
+
+        /**
+         * Creates end point for
+         * @param origin Start point
+         * @param rotation Rotation (degrees)
+         * @param len Length
+         *
+         */
         private Point2D endPoint(Point2D origin, double rotation, double len) {
             double x = origin.getX();
             double y = origin.getY();
--- src/sample/RightMenuVbox.java
+++ src/sample/RightMenuVbox.java
@@ -1,39 +1,37 @@
 package sample;
 
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
+
 public class RightMenuVbox extends VBox {
 
-    private final Slider randomSlider = new Slider(0,4,0);
+    private final Slider randomSlider = new Slider(0,3,0);
     private final TextField lengthTf = new TextField();
     private final TextField orderTf = new TextField();
-    private final Button btnOk = new Button("Ok");
-    private final TextField sizeTf = new TextField();
+    private final Button btnOk = new Button("Ok! Go!");
+    private final CheckBox discoCb = new CheckBox();
 
     private static final int TF_WIDTH = 70;
 
     public RightMenuVbox() {
 
         randomSlider.setMajorTickUnit(1);
+        randomSlider.setMinorTickCount(0);
         randomSlider.setShowTickMarks(true);
         randomSlider.setShowTickLabels(true);
         randomSlider.setSnapToTicks(true);
+
 
         lengthTf.setText("100");
         lengthTf.setMaxWidth(TF_WIDTH);
         orderTf.setText("0");
         orderTf.setMaxWidth(TF_WIDTH);
-        sizeTf.setText("0");
-        sizeTf.setMaxWidth(TF_WIDTH);
 
 
         this.setSpacing(5);
@@ -45,7 +43,7 @@ public class RightMenuVbox extends VBox {
                 new Label("Grad random:"), randomSlider,
                 new Label("Lengde (stamme): "), lengthTf,
                 new Label("Nivå: "), orderTf,
-                new Label("Størrelse:"), sizeTf,
+                new Label("Disco?"), discoCb,
                 btnOk
 
         );
@@ -73,15 +71,12 @@ public class RightMenuVbox extends VBox {
         return 0.0;
     }
 
-    public double getSize(){
-        if (!sizeTf.getText().trim().isEmpty()) {
-            return Double.parseDouble(sizeTf.getText());
-        }
-        return 0.0;
-    }
 
     public double getRandomSliderValue() {
         return randomSlider.getValue();
     }
 
+    public CheckBox getDiscoCb() {
+        return discoCb;
+    }
 }
